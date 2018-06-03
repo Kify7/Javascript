91516f441a9b63adfc3262c283649b67eb91b524
Unknown <kflores.valona@gmail.com>
2018-06-02 23:41:40 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,100 @@
+// tipos de datos
+/* 
+string  cadena de Texto
+number  numero
+boolean true or false
+
+undefined
+null
+*/
+
+/* alert("Hello World");     */
+
+/* Variables */
+
+/* var nombre = "Kify"
+var edad = 30;
+var teGustaElAnime = true; 
+ */
+/* Estructura de Datos */
+
+/* Arreglos: los arreglos empiezan en cero  */
+/* var x = ["Paola","Marco","Karla","Edgar","Claudia","Margarita","Josue"];
+alert(x[2]); */
+
+/* Jsons u Objetos {} */
+
+/* var alumno = { "nombre": "Kify","edad": 30,"telefono": 56575 };
+
+alert (alumno["nombre"]);
+alert (alumno.nombre = "wuwu");
+
+ */
+
+ /*  console.log() USANDO INSPECCION EN NAVEGADOR*/
+ 
+console.log (20 == 20);
+// console.log (20 = 21);
+// console.log (20 = 22);
+
+
+// var nombre = "kify";
+// console.log(nombre == "kify");
+
+// var edad = 20
+// console.log(edad = 20);
+// console.log(edad = 40);
+// console.log(edad = 15);
+// console.log(edad = 17);
+// console.log(edad = 30);
+
+// console.log(edad > 18 && edad < 30);
+// console.log(edad > 18 || edad < 30);
+
+/* var edad1 = 15
+var edad2 = 13
+var edad3 = 40
+var edad4 = 35
+var edad5 = 128
+
+console.log(edad1 > 18 && edad1 < 30);
+console.log(edad2 > 18 && edad2 < 30);
+console.log(edad3 > 18 && edad3 < 30);
+console.log(edad4 > 18 && edad4 < 30);
+console.log(edad5 > 18 && edad5 < 30);
+
+ */
+
+/* Declarar Variable en funcion : las funciones son pedazos de código que podemos mandar llamar */
+
+/* function comparar () {
+    alert("salu2");
+}
+
+ /* EJECUTAR FUNCIONES */
+
+// comparar()
+
+/* Declarar Variables */
+
+/* var edad1 = 15;
+var edad2 = 16;
+var edad3 = 25; */
+ 
+/* Declarar Funcion */
+
+/* function comparar(x) { console.log( x > 18 && x < 30);
+} */
+
+/* Ejecutar Funciones */
+
+/* comparar(edad1);
+comparar(edad2);
+comparar(edad3);
+comparar(edad1 + edad2); */
+
+/* Se Pueden Declarar Variables dentro de las Funciones */
+
+function comparar(x) { console.log( x > 18 && x < 30);
+    var zz = 15
+}
